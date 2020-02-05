c37d56bad0e212335bc1844f8f1294c27ba1fca6
Rabah Hammoutene <44506890+yacouzz@users.noreply.github.com>
2020-02-05 16:05:06 +0100

    Update Main.js

--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -13,9 +13,9 @@ class Main extends React.Component {
                 <div className="row justify-content-center">
                     <div className="col-md-8">
                         <div className="card">
-                            <div className="card-header">Example Component</div>
+                            <div className="card-header">Dashboard</div>
 
-                            <div className="card-body">I'm an example component!</div>
+                            <div className="card-body">You're Logged in!</div>
                         </div>
                     </div>
                 </div>
